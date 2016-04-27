39ccc61107839b207d24e118c208790090261727
kenneth peralta <kenneth.peralta@hotmail.com>
2016-04-26 20:07:21 -0700

    added two users to backendless

--- public_html/js/todolist.js
+++ public_html/js/todolist.js
@@ -1,7 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function () {
+var APPLICATION_ID = "27F891D9-CEAA-A7C4-FF38-01C51372B800",
+    SECRET_KEY = "398F423D-9AF2-4ED7-FF89-1A8AE5F55A00",
+    VERSION = "v1";
+  
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var dataStore = Backendless.Persistence.of(Posts);
+    var post = new Posts({title: "Todo List", content: " 1st Task:Finish the project", email:"email@email.com"});
+    dataStore.save(post);
+    
+});
+
+ function Posts(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
 
 
